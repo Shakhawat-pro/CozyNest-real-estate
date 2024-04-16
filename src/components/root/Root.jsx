a8831e66e4356bdea539acca2b4c443f30ece7b5
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-04-17 02:11:07 +0600

    footer done

--- src/components/root/Root.jsx
+++ src/components/root/Root.jsx
@@ -1,3 +1,4 @@
+import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar";
 import { Outlet } from "react-router-dom";
 
@@ -6,6 +7,7 @@ const Root = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
