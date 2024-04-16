5def80dc544058de8a0a2d386ea25d3e73465a82
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-04-17 00:34:23 +0600

    slider done done

---
+++ src/components/home/Slider.jsx
@@ -0,0 +1,68 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle"
+import 'swiper/css'
+import "./styles.css"
+
+import { Navigation, Autoplay, Pagination} from 'swiper/modules';
+
+const Slider = () => {
+    return (
+        <div className="w-11/12 mx-auto rounded-3xl overflow-clip z-1 mt-16 lg:mt-10">
+
+            <Swiper
+                    style={{
+                        '--swiper-navigation-color': '#23BE0A',
+                        '--swiper-pagination-color': '#23BE0A',
+                    }}
+             navigation={true} 
+             autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+            modules={[Navigation, Autoplay, Pagination]}
+            loop={true}
+            >
+                <SwiperSlide>
+                    <div className="slide slide-1  flex flex-col ">
+                        <h1 className='text-[#23BE0A] text-2xl lg:text-7xl font-bold mb-6'>Urban Oasis</h1>
+                        <p className=" w-10/12 text-sm lg:text-md text-white max-sm:hidden">Immerse yourself in the vibrant pulse of city life with our urban oasis collection. Discover stylish apartments, trendy lofts, and chic condominiums nestled in the heart of bustling metropolises. Experience the convenience of city living without compromising on comfort and style.</p>
+                        <p className="w-9/12 text-sm lg:text-md text-white md:hidden">Experience vibrant city life with stylish apartments and trendy lofts in our Urban Oasis collection.</p>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide slide-2 flex flex-col ">
+                        <h1 className='text-[#23BE0A] text-2xl lg:text-7xl font-bold mb-6'>Seaside Serenity</h1>
+                        <p className=" w-10/12 text-sm lg:text-md text-white max-sm:hidden">Escape to the tranquil shores and embrace the serenity of coastal living. Dive into our seaside serenity collection featuring beachfront villas, cozy cottages, and elegant waterfront properties. Let the sound of waves and salty breeze rejuvenate your soul as you unwind in your own piece of paradise.</p>
+                        <p className="w-9/12 text-sm lg:text-md text-white md:hidden">Escape to tranquil shores and embrace coastal living in our Seaside Serenity collection.</p>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide slide-3 flex flex-col ">
+                        <h1 className='text-[#23BE0A] text-2xl lg:text-7xl font-bold mb-6'>Suburban Charm</h1>
+                        <p className=" w-10/12 text-sm lg:text-md text-white max-sm:hidden">Experience the warmth of community and the comfort of home in our suburban charm collection. Discover family-friendly neighborhoods, cozy townhouses, and spacious suburban estates surrounded by lush greenery and amenities. Live the quintessential suburban lifestyle where convenience meets tranquility. </p>
+                        <p className="w-9/12 text-sm lg:text-md text-white md:hidden">Find warmth and comfort in family-friendly neighborhoods and cozy townhouses with our Suburban Charm collection.</p>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="slide slide-4 flex flex-col ">
+                        <h1 className='text-[#23BE0A] text-2xl lg:text-7xl font-bold mb-6'>Mountain Retreat</h1>
+                        <p className=" w-10/12 text-sm lg:text-md text-white max-sm:hidden">Embark on an adventure to the mountains and indulge in the beauty of nature with our mountain retreat collection. Explore rustic cabins, luxurious chalets, and secluded lodges nestled amidst breathtaking alpine landscapes. Whether your an outdoor enthusiast or seeking peace and solitude, our mountain retreats offer the perfect escape.</p>
+                        <p className="w-9/12 text-sm lg:text-md text-white md:hidden">Embark on a mountain adventure and indulge in nature beauty with our Mountain Retreat collection.</p>
+                    </div>
+
+                </SwiperSlide>
+
+            </Swiper>
+
+
+        </div>
+    );
+};
+
+export default Slider;
+
+
+
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import { Link, NavLink } from "react-router-dom";
 import './navbar.css'
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
+import 'animate.css';
+
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext)
@@ -9,32 +11,32 @@ const Navbar = () => {
         logOut()
     }
     const navLinks = <>
-        <li className="active:bg-white"><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/residents">Residents</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
-        <li><NavLink to="/profile">Profile</NavLink></li>
+        <li className="animate__animated animate__bounceInUp"><NavLink to="/">Home</NavLink></li>
+        <li className="animate__animated animate__bounceInUp"><NavLink to="/residents">Residents</NavLink></li>
+        <li className="animate__animated animate__bounceInUp"><NavLink to="/contact">Contact</NavLink></li>
+        <li className="animate__animated animate__bounceInUp"><NavLink to="/profile">Profile</NavLink></li>
     </>
     return (
-        <div className="lg:w-11/12 mx-auto">
-            <div className="navbar bg-base-100">
+        <div className="lg:w-11/12 mx-auto animate__backInDown pt-4 rounded-xl">
+            <div className="navbar bg-base-100 rounded-xl">
                 <div className="navbar-start">
-                    <div className="dropdown">
+                    <div className="dropdown animate__animated animate__bounceInUp">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu-sm dropdown-content mt-3 z-[1] p-4 space-y-5 shadow bg-white rounded-box w-52">
+                        <ul tabIndex={0} className="menu-sm dropdown-content flex items-center mt-3 z-10 p-4 shadow bg-white rounded-box w-[350px]">
                             {navLinks}
                         </ul>
                     </div>
-                    <img src="/favicon.png" alt="" />
-                    <a className="btn btn-ghost text-2xl font-bold text-[#55d142]">CozyNest</a>
+                    <img className="animate__animated animate__bounceInUp" src="/favicon.png" alt="" />
+                    <a className="animate__animated animate__bounceInUp animated-delay-2s  btn btn-ghost text-2xl font-bold text-[#55d142]">CozyNest</a>
                 </div>
                 <div className="navbar-center hidden  lg:flex">
                     <ul className="menu-horizontal bg-white px-1 space-x-5">
                         {navLinks}
                     </ul>
                 </div>
-                <div className="navbar-end">
+                <div className="navbar-end animate__animated animate__bounceInUp">
                     {
                         user ? <>
                             <div className="flex items-center space-x-6">
