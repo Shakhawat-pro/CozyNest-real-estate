e7a667cce38d3983bf5ff8fe215d8147ddbf62d1
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-04-17 16:12:09 +0600

    .env  done

--- src/components/firebase/firebase.config.js
+++ src/components/firebase/firebase.config.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCfIaVNixgzSWHd6hN1jUofV5D6-W1_lKY",
-  authDomain: "cozynest-7b9c3.firebaseapp.com",
-  projectId: "cozynest-7b9c3",
-  storageBucket: "cozynest-7b9c3.appspot.com",
-  messagingSenderId: "503590901470",
-  appId: "1:503590901470:web:d034c54cb65acf20b8544f"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
